e73c172640d97cdb81f6ebf29d956faed50a49db
jaupi98 <alessio.jaupllari@maxwell.mi.it>
2023-05-27 17:16:19 +0200

    finito

--- js/script.js
+++ js/script.js
@@ -9,12 +9,12 @@ const images =[
 ]
 // 2 MI RECPERO GLI ELEMENTI DEL DOM CON CLASSI ITEMAS E THUMS NEI QUALI ANDARE AD INNIETTAE HTML DELLE IMMAGINI
 
-const items = documnet.querySelector('.items');
-const thumbs = document.querySelector('.thumbs');
+const items = document.querySelector('.items');
+const thumbs = document.querySelector('.thumbs-content');
 
 //  DEFINIRE 2 VARIABILI : UNA CHE CONTIENE LA STTRINGA DELLE IMMAGINI IN TEEMS E L'ALTRA IN THUMBS.
 
-let itemasContent='';
+let itemesContent='';
 let thumbsContent='';
 
 // DEFINICO L'INDICE DELL'ARRAY CHE DEVE RAPPRESENTARE LIMMAGINE ATTIVA
@@ -22,6 +22,42 @@ let thumbsContent='';
 let activeItems = 0;
 
 // ciclo l'arrey per riempire le stringhe con l'html delle immagini
-for (let i=0, i < images.length, i++){
+for (let i=0 ;i < images.length ;i++){
     
+    itemesContent +=`<div class="item">
+                        <img src="./${images[i]}" alt="">
+                    </div>`;
+    
+    thumbsContent +=` <div class="thumb">
+                    <img src="./${images[i]}" alt="">
+                </div>`;
+
 }
+// inietto in items e thumbs e thumb lhtml creato
+items.innerHTML = itemesContent;
+thumbs.innerHTML = thumbsContent;
+
+// APLLICO LA VARIABILE ACTIVEITEMS PER APLLICARE LA CLASSE ACTIVE
+document.getElementsByClassName('item')[activeItems].classList.add('active');
+document.getElementsByClassName('thumb')[activeItems].classList.add('active');
+
+const prev = document.querySelector('.prev');
+const next = document.querySelector('.next');
+
+// gestisco il click sul div con lasse prev
+
+prev.addEventListener('click' , function(){
+    if(activeItems === 0){
+        activeItems= images.length -1 ;
+
+    } 
+    else{
+        activeItems--;
+    }
+
+    document.querySelector('.item.active').classList.remove('active');
+    document.getElementsByClassName('item')[activeItems].classList.add('active');
+
+    document.querySelector('.thumb.active').classList.remove('active');
+    document.getElementsByClassName('thumb')[activeItems].classList.add('active')
+});
